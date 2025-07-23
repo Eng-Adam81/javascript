3921eac1a30d2464bd480c8e49281ef9ca6f4b85
Eng-Adam81 <cigaal.aa97@gmail.com>
2025-07-23 07:43:36 -0400

    I have added dom and updated my javascript lesson

---
+++ loops/scrip.js
@@ -0,0 +1,41 @@
+// console.log ("Aden")
+// console.log ("Aden")
+// for(let index = 0; index < 100; index++){
+//     console.log("Aden", index);
+// }
+
+// let i = 0
+// while(  i < 100 ){
+//     console.log("miski", i);
+//     i++
+// }
+
+// let i = 0
+// do {
+//     console.log("miski", i);
+//     i++
+// } while(  i < 100 )
+
+
+//     for(let i = 0; i < 100; i++){
+    
+//     if(i==4){
+//         continue
+//     //    break
+//     }
+//     console.log("Aden", i);
+// }
+
+
+
+// Example 2: for...of loop (used to iterate over iterable objects like arrays)
+var fruits = ["Banana", "Apple", "Mango"];
+
+console.log("\nUsing for...of:");
+for (var fruit of fruits) {
+  console.log(fruit);
+}
+// Output:
+// Banana
+// Apple
+// Mango
